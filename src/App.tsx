b0b4b8b1a993a075afacd9fa110cff26445c0ab3
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-23 11:25:43 +0300

    Added shadcn library components and config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
 
     return (
         <>
+            <Button>SHADCN Button</Button>
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
