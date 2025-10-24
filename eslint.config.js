c97f9e1a71b9bb51049b0b7a598a245cd1ed3457
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-24 10:45:05 +0300

    Added better config to include the prettierrc file

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import unusedImports from "eslint-plugin-unused-imports";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import unusedImports from 'eslint-plugin-unused-imports';
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'unused-imports': unusedImports,
+            'prettier': pluginPrettier,
+
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'unused-imports/no-unused-imports': 'warn',
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        },
     },
-    plugins: {
-      "unused-imports": unusedImports,
+    {
+        files: ['@shadcn/**/*.{ts,tsx}'],
+        // Is being merged with the above configuration so it will apply the prettier
+        // plugins: {  
+        //     'prettier': pluginPrettier,
+        // },
+        rules: {
+            // '@typescript-eslint/no-unused-vars': 'off',
+            // 'no-unused-vars': 'off',
+            // 'unused-imports/no-unused-imports': 'off',
+            'react-refresh/only-export-components': 'off',
+            // 'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        },
     },
-    rules: {
-      'semi': ['error', 'always'],
-      'indent': ['error', 4, { SwitchCase: 1 }],
-      'quotes': ['error', 'single'],
-      
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      "unused-imports/no-unused-imports": "warn",
-    }
-  }, {
-    ignores: ['dist/', 'node_modules/']
-  }
-])
+]);
