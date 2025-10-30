12583b7b0cb98fe0c95f8e93d83038620b071385
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-10-30 16:52:23 +0200

    Added basic profile button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,12 +4,11 @@ import { Bell, Search, Menu } from 'lucide-react';
 import { ThemeToggle } from '../common/ThemeToggle';
 import { Link } from 'react-router';
 import HeaderAuthButtons from './HeaderAuthButtons';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import { useAuth } from '@/context/AuthContext';
 
-const Header = () => {	
-
-	
-
-
+const Header = () => {
+	const { user } = useAuth();
 	return (
 		<header className="sticky top-0 z-50 w-full border-b border-border bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-card/60">
 			<div className="container mx-auto px-4 max-w-7xl">
@@ -76,6 +75,8 @@ const Header = () => {
 
 					{/* Right Actions */}
 					<div className="flex items-center gap-3">
+
+
 						<Button
 							variant="ghost"
 							size="icon"
@@ -93,9 +94,12 @@ const Header = () => {
 							<Search className="h-5 w-5" />
 						</Button>
 
+						<ThemeToggle />
+
 						<div className="hidden md:flex items-center gap-3">
-							<HeaderAuthButtons/>
+							<HeaderAuthButtons />
 						</div>
+
 
 						<Button
 							variant="ghost"
@@ -105,7 +109,26 @@ const Header = () => {
 							<Menu className="h-5 w-5" />
 						</Button>
 
-						<ThemeToggle />
+
+						{user?.username && <>
+							<Link to="/discover" className="flex items-center">
+								<Button
+									variant="ghost"
+									size="icon"
+									className="cursor-pointer"
+								>
+									<Avatar >
+										<AvatarImage
+											className='rounded-md'
+											src="https://cdn.shadcnstudio.com/ss-assets/avatar/avatar-5.png"
+											alt="Hallie Richards"
+										/>
+										<AvatarFallback className="text-xs">HR</AvatarFallback>
+									</Avatar>
+								</Button>
+							</Link>
+						</>}
+
 					</div>
 				</div>
 			</div>
