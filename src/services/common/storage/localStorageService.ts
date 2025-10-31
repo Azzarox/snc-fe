fe7a1adb839fcd84374e97b286b6df43e63f2ccd
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-11-01 01:11:31 +0200

    Refactored storage

--- src/services/common/storage/localStorageService.ts
+++ src/services/common/storage/localStorageService.ts
@@ -1,22 +1,39 @@
 import { $common } from "@/schemas/common/helpers";
-import storage from "./storage";
+import storage, { StorageKeys } from "./storage";
+import type { Theme } from "@/context/ThemeContext";
 
-const ACCESS_TOKEN_KEY = 'accessToken';
 
 const getAccessToken = () => {
-    return storage.get(ACCESS_TOKEN_KEY, $common.string);
+    return storage.get(StorageKeys.ACCESS_TOKEN_KEY);
 }
 
-const setAccessToken = (value: string) => {
-    storage.set(ACCESS_TOKEN_KEY, value)
+const setAccessToken = (value:string) => {
+    storage.set(StorageKeys.ACCESS_TOKEN_KEY, value)
 }
 
 const removeAccessToken = () => {
-    return storage.remove(ACCESS_TOKEN_KEY);
+    return storage.remove(StorageKeys.ACCESS_TOKEN_KEY);
+}
+
+//
+
+const getTheme = () => {
+    return storage.get(StorageKeys.THEME)
+}
+
+const setTheme = (value: Theme) => {
+     storage.set(StorageKeys.THEME, value)
+}
+
+const removeTheme = () => {
+     storage.remove(StorageKeys.THEME)
 }
 
 export const localStorageService = {
     getAccessToken,
     setAccessToken,
     removeAccessToken,
+    getTheme,
+    setTheme,
+    removeTheme
 }
--- src/services/common/storage/storage.ts
+++ src/services/common/storage/storage.ts
@@ -1,23 +1,47 @@
-import type z from "zod";
+import { Theme } from "@/context/ThemeContext";
+import { z } from "zod";
+
+
+export enum StorageKeys {
+    ACCESS_TOKEN_KEY = 'accessToken',
+    THEME = 'vite-ui-theme'
+}
+
+const storageSchema = {
+    [StorageKeys.ACCESS_TOKEN_KEY]: z.string(),
+    [StorageKeys.THEME]: z.enum([Theme.DARK, Theme.LIGHT, Theme.SYSTEM]),
+} as const;
+
+type StorageSchemas = typeof storageSchema;
+
+type StorageTypes = {
+    [K in keyof StorageSchemas]: z.infer<StorageSchemas[K]>;
+};
 
 const storage = {
-    get<S extends z.ZodSchema>(key: string, schema: S): z.infer<S> | null {
+    get<K extends keyof StorageTypes>(
+        key: K,
+    ): StorageTypes[K] | null {
         const item = localStorage.getItem(key);
         if (!item) return null;
         try {
             const parsed = JSON.parse(item);
+            const schema = storageSchema[key];
             const result = schema.safeParse(parsed);
-            return result.success ? result.data : null;
+            return result.success ? (result.data as StorageTypes[K]) : null;
         } catch {
             return null;
         }
     },
 
-    set<T>(key: string, value: T): void {
+    set<K extends keyof StorageTypes>(
+        key: K,
+        value: StorageTypes[K]
+    ): void {
         localStorage.setItem(key, JSON.stringify(value));
     },
 
-    remove(key: string): void {
+    remove(key: keyof StorageTypes): void {
         localStorage.removeItem(key);
     },
 
