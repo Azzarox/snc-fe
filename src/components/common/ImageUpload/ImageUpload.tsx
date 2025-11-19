3766d7aa8a39c7a638a7ab9b4d1becf446942796
NikolaBaikushev <234618369+NikolaBaikushev@users.noreply.github.com>
2025-11-19 10:52:40 +0200

    Fixed comments

--- src/components/common/ImageUpload/ImageUpload.tsx
+++ src/components/common/ImageUpload/ImageUpload.tsx
@@ -1,57 +1,6 @@
-import {
-	type ReactNode,
-	type FC,
-	type ForwardRefExoticComponent,
-	type RefAttributes,
-} from 'react';
+import { type ReactNode } from 'react';
 import { ImageUploadProvider } from './context/ImageUploadContext';
 import type { ImageUploadOptions } from '@/hooks/useImageUpload';
-import type { ModalImperativeHandle } from '@/types/common/ModalImpretiveHandle';
-import type { ApiResponse } from '@/types/api/response';
-
-type ImageUploadProps = {
-	children: ReactNode;
-	options?: ImageUploadOptions;
-};
-
-type PreviewProps = {
-	currentImageUrl?: string;
-	imageClassName?: string;
-};
-
-type CropperPreviewProps = {
-	aspectRatio?: number;
-};
-
-type ModalProps = {
-	title?: string;
-	description?: string;
-	currentImageUrl?: string;
-	imageClassName?: string;
-	onSuccess: () => void;
-	uploadFn: (formData: FormData) => Promise<ApiResponse<any>>;
-	successMessage?: string;
-	uploadOptions?: ImageUploadOptions;
-	onReset?: () => Promise<void>;
-	showResetButton?: boolean;
-};
-
-type ImageUploadComponent = FC<ImageUploadProps> & {
-	Preview: FC<PreviewProps>;
-	CropperPreview: FC<CropperPreviewProps>;
-	UploadZone: FC;
-	FileInput: FC;
-	ErrorMessage: FC;
-	Modal: ForwardRefExoticComponent<
-		ModalProps & RefAttributes<ModalImperativeHandle>
-	>;
-};
-
-export const ImageUpload = (({ children, options }: ImageUploadProps) => {
-	return (
-		<ImageUploadProvider options={options}>{children}</ImageUploadProvider>
-	);
-}) as ImageUploadComponent;
 
 import Preview from './Preview';
 import CropperPreview from './CropperPreview';
@@ -59,6 +8,17 @@ import UploadZone from './UploadZone';
 import FileInput from './FileInput';
 import ErrorMessage from './ErrorMessage';
 import Modal from './Modal';
+
+type ImageUploadProps = {
+	children: ReactNode;
+	options?: ImageUploadOptions;
+};
+
+export function ImageUpload({ children, options }: ImageUploadProps) {
+	return (
+		<ImageUploadProvider options={options}>{children}</ImageUploadProvider>
+	);
+}
 
 ImageUpload.Preview = Preview;
 ImageUpload.CropperPreview = CropperPreview;
